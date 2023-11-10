d250de812c42e301bff826995d0c12740056b828
didac.martin <didacmartin@gmail.com>
2023-11-10 10:01:58 +0100

    add nuevos cambios

--- app.js
+++ app.js
@@ -10,21 +10,32 @@ app.use(bodyParser.json());
 
 app.get('/sumar-puntos/:pass_id', async (req, res) => {
     try {
-        const passUid = req.params.passUid;
+        const passUid = req.params.pass_id;
         const apiUrl = `https://app.passcreator.com/api/pass/${passUid}?zapierStyle=true`;
+        console.info(apiUrl);
         const apiKey = process.env.API_KEY;
+        const storedValue = Math.floor(Math.random() * 8) + 1;
 
         const response = await axios.post(apiUrl, req.body, {
             headers: {
                 'Authorization': apiKey,
                 'Content-Type': 'application/json'
-            }
-        });
+            },
+            params: { storedValue:storedValue } // Pasa el campo storedValue como parámetro
 
-        res.status(response.status).json(response.data);
+        });
+        // Devuelve el código de estado y el cuerpo de la respuesta
+        res.status(response.status).json({
+            statusCode: response.status,
+            body: response.data
+        });
     } catch (error) {
         console.error('Error en la llamada POST:', error.message);
-        res.status(500).send('Error interno del servidor');
+        // Devuelve un código de estado 500 y un mensaje de error
+        res.status(500).json({
+            statusCode: 500,
+            body: 'Error interno del servidor'
+        });
     }
 });
 
