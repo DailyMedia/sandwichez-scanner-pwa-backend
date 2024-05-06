353acb4fc6b39b4ddba9295ef8cf231ff7b048fb
DailyMedia <info@dailymedia.io>
2024-05-06 07:30:03 +0200

    set cors on main function

--- app.js
+++ app.js
@@ -10,6 +10,15 @@ const port = 3000;
 app.use(bodyParser.json());
 app.use(cors());
 app.get('/sumar-puntos/:pass_id', async (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
     try {
         const passUid = req.params.pass_id;
         const apiUrl = `https://app.passcreator.com/api/pass/${passUid}?zapierStyle=true`;
@@ -73,30 +82,6 @@ app.get('/sumar-puntos/:pass_id', async (req, res) => {
         });
     }
 });
-
-const allowCors = fn => async (req, res) => {
-    res.setHeader('Access-Control-Allow-Credentials', true)
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    // another common pattern
-    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
-    if (req.method === 'OPTIONS') {
-      res.status(200).end()
-      return
-    }
-    return await fn(req, res)
-  }
-  
-const handler = (req, res) => {
-    const d = new Date()
-    res.end(d.toString())
-}
-
-module.exports = allowCors(handler)
 
 app.listen(port, () => {
     console.log(`Servidor Express escuchando en el puerto ${port}`);
